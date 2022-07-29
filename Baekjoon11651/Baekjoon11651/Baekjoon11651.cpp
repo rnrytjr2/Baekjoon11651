f650ab3d772b80efae8f002aee8ccd0a2f1f6954
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-07-29 23:19:26 +0900

    solve
    c++

---
+++ Baekjoon11651/Baekjoon11651/Baekjoon11651.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int length;
+    cin >> length;
+    vector<vector<int>> datas(length, vector<int> (2, 0));
+    for (int i = 0; i < length; i++)
+    {
+        cin >> datas[i][1] >> datas[i][0];
+    }
+    sort(datas.begin(), datas.end());
+    for (int i = 0; i < length; i++)
+    {
+        cout << datas[i][1] << " " << datas[i][0] << "\n";
+    }
+}
